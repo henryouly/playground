e61b6d5c35c756689745c67b5d55eda82b1257fb
Henry Ou <henryouly@gmail.com>
2024-05-19 00:10:45 -0700

    Fake loading

--- stock-ai/components/chat-messages.tsx
+++ stock-ai/components/chat-messages.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChatMessage, ChatMessageProps } from "@/components/chat-message";
+import { useEffect, useState } from "react";
 
 interface ChatMessagesProps {
   messages: ChatMessageProps[];
@@ -11,13 +12,23 @@ export const ChatMessages = ({
   messages = [],
   isLoading
 }: ChatMessagesProps) => {
+  const [fakeLoading, setFakeLoading] = useState(messages.length === 0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
+        isLoading={fakeLoading}
         role="system"
-        content={`Hello`} />
-      <ChatMessage
-        role="user"
         content={`Hello`} />
     </div>
   )
