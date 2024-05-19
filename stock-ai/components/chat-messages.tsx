bf31bbf852503525895efaeebc2833bfb3d7e928
Henry Ou <henryouly@gmail.com>
2024-05-19 00:16:11 -0700

    Complete chat message.

--- stock-ai/components/chat-messages.tsx
+++ stock-ai/components/chat-messages.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChatMessage, ChatMessageProps } from "@/components/chat-message";
-import { useEffect, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
   messages: ChatMessageProps[];
@@ -12,6 +12,7 @@ export const ChatMessages = ({
   messages = [],
   isLoading
 }: ChatMessagesProps) => {
+  const scrollRef = useRef<ElementRef<"div">>(null);
   const [fakeLoading, setFakeLoading] = useState(messages.length === 0);
 
   useEffect(() => {
@@ -24,12 +25,32 @@ export const ChatMessages = ({
     }
   }, []);
 
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
         isLoading={fakeLoading}
         role="system"
-        content={`Hello`} />
+        content={`Hello, I'm a stock assistant.`} />
+      {
+        messages.map((message) => (
+          <ChatMessage
+            key={message.content}
+            role={message.role}
+            content={message.content}
+          />
+        ))
+      }
+      {isLoading && (
+        <ChatMessage
+          role="system"
+          isLoading
+        />
+      )}
+      <div ref={scrollRef} />
     </div>
   )
 }
