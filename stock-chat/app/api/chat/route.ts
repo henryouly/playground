91bad66ee1e42951a4f5c9356577358db75bec65
Henry Ou <henryouly@gmail.com>
2024-05-19 23:02:13 -0700

    Use Replicate from Replicate package directly

--- stock-chat/app/api/chat/route.ts
+++ stock-chat/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import  { StreamingTextResponse } from "ai";
 
-import { Replicate } from "@langchain/community/llms/replicate";
+import Replicate from "replicate";
 import { NextResponse } from "next/server";
 
 import MemoryManager from "@/lib/memory";
@@ -26,21 +26,13 @@ export async function POST(
     await memoryManager.writeToHistory("User: " + prompt + "\n");
     const recentChatHistory = await memoryManager.readLatestHistory();
 
-    const model = new Replicate({
-      model:
-        "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
-      input: {
-        max_length: 2048,
-      },
-      apiKey: process.env.REPLICATE_API_TOKEN,
-    });
+    const model = new Replicate();
 
-    model.verbose = true;
-
-    const resp = String(
-      await model
-        .invoke(
-          `
+    const output = await model.run(
+      "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+      {
+        input: {
+          prompt:  `
           ONLY generate NO more than three sentences as ${name}. DO NOT generate more than three sentences. 
           Make sure the output you generate starts with '${name}:' and ends with a period.
   
@@ -50,9 +42,11 @@ export async function POST(
          ${relevantHistory}
   
          ${recentChatHistory}\n${name}:`
-        )
-        .catch(console.error)
-    );
+        }
+      }
+    ) as string[];
+
+    const resp = output.join("");
 
     // Right now just using super shoddy string manip logic to get at
     // the dialog.
