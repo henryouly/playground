691fc36544078c638de55b8b88486cd6af579349
henryouly <henryouly@gmail.com>
2024-05-29 23:43:22 +0000

    Sidebar

--- stock-ai/components/sidebar.tsx
+++ stock-ai/components/sidebar.tsx
@@ -1,22 +1,243 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { MoreHorizontal, SquarePen, Trash2 } from "lucide-react";
 import { Message } from "ai/react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useLocalStorageData } from "@/lib/use-local-storage-data";
 
 
-type Props = {
-  chatId?: string;
+interface SidebarProps {
   isCollapsed: boolean;
-  isMobile: boolean;
   messages: Message[];
-  setMessages?: (messages: Message[]) => void;
+  onClick?: () => void;
+  isMobile: boolean;
+  chatId: string;
+  setMessages: (messages: Message[]) => void;
 }
 
-export const Sidebar = ({
-  chatId,
-}: Props) => {
+const SidebarSkeleton = () => {
   return (
-    <div>
-      Sidebar
+    <div className="flex flex-col w-full gap-2 ">
+      <div className="flex h-14 w-full bg-primary/5 justify-between rounded-xl items-center p-2">
+        <Skeleton className="h-6 w-2/3 rounded-sm" />
+        <Skeleton className="h-6 w-6 rounded-full" />
+      </div>
+
+      <div className="flex h-14 w-full bg-primary/5 opacity-80 justify-between rounded-xl items-center p-2">
+        <Skeleton className="h-6 w-2/3 rounded-sm" />
+        <Skeleton className="h-6 w-6 rounded-full" />
+      </div>
+
+      <div className="flex h-14 w-full bg-primary/5 opacity-60 justify-between rounded-xl items-center p-2">
+        <Skeleton className="h-6 w-2/3 rounded-sm" />
+        <Skeleton className="h-6 w-6 rounded-full" />
+      </div>
+
+      <div className="flex h-14 w-full bg-primary/5 opacity-40 justify-between rounded-xl items-center p-2">
+        <Skeleton className="h-6 w-2/3 rounded-sm" />
+        <Skeleton className="h-6 w-6 rounded-full" />
+      </div>
+
+      <div className="flex h-14 w-full bg-primary/5 opacity-20 justify-between rounded-xl items-center p-2">
+        <Skeleton className="h-6 w-2/3 rounded-sm" />
+        <Skeleton className="h-6 w-6 rounded-full" />
+      </div>
+      
     </div>
-  )
+  );
+};
+
+export function Sidebar({
+  messages,
+  isCollapsed,
+  isMobile,
+  chatId,
+  setMessages,
+}: SidebarProps) {
+  const [localChats, setLocalChats] = useState<
+    { chatId: string; messages: Message[] }[]
+  >([]);
+  const localChatss = useLocalStorageData("chat_", []);
+  const [selectedChatId, setSselectedChatId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (chatId) {
+      setSselectedChatId(chatId);
+    }
+
+    setLocalChats(getLocalstorageChats());
+    const handleStorageChange = () => {
+      setLocalChats(getLocalstorageChats());
+    };
+    window.addEventListener("storage", handleStorageChange);
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  const getLocalstorageChats = (): {
+    chatId: string;
+    messages: Message[];
+  }[] => {
+    const chats = Object.keys(localStorage).filter((key) =>
+      key.startsWith("chat_")
+    );
+
+    if (chats.length === 0) {
+      setIsLoading(false);
+    }
+
+    // Map through the chats and return an object with chatId and messages
+    const chatObjects = chats.map((chat) => {
+      const item = localStorage.getItem(chat);
+      return item
+        ? { chatId: chat, messages: JSON.parse(item) }
+        : { chatId: "", messages: [] };
+    });
+
+    // Sort chats by the createdAt date of the first message of each chat
+    chatObjects.sort((a, b) => {
+      const aDate = new Date(a.messages[0].createdAt);
+      const bDate = new Date(b.messages[0].createdAt);
+      return bDate.getTime() - aDate.getTime();
+    });
+
+    setIsLoading(false);
+    return chatObjects;
+  };
+
+  const handleDeleteChat = (chatId: string) => {
+    localStorage.removeItem(chatId);
+    setLocalChats(getLocalstorageChats());
+  };
+
+  return (
+    <div
+      data-collapsed={isCollapsed}
+      className="relative justify-between group lg:bg-accent/20 lg:dark:bg-card/35 flex flex-col h-full gap-4 p-2 data-[collapsed=true]:p-2 "
+    >
+      <div className=" flex flex-col justify-between p-2 max-h-fit overflow-y-auto">
+        <Button
+          onClick={() => {
+            router.push("/");
+            // Clear messages
+            setMessages([]);
+          }}
+          variant="ghost"
+          className="flex justify-between w-full h-14 text-sm xl:text-lg font-normal items-center "
+        >
+          <div className="flex gap-3 items-center ">
+            {!isCollapsed && !isMobile && (
+              <Image
+                src="/ollama.png"
+                alt="AI"
+                width={28}
+                height={28}
+                className="dark:invert hidden 2xl:block"
+              />
+            )}
+            New chat
+          </div>
+          <SquarePen size={18} className="shrink-0 w-4 h-4" />
+        </Button>
+
+        <div className="flex flex-col pt-10 gap-2">
+          <p className="pl-4 text-xs text-muted-foreground">Your chats</p>
+          {localChats.length > 0 && (
+            <div>
+              {localChats.map(({ chatId, messages }, index) => (
+                <Link
+                  key={index}
+                  href={`/${chatId.substr(5)}`}
+                  className={cn(
+                    {
+                      [buttonVariants({ variant: "secondary" })]:
+                        chatId.substring(5) === selectedChatId,
+                      [buttonVariants({ variant: "ghost" })]:
+                        chatId.substring(5) !== selectedChatId,
+                    },
+                    "flex justify-between w-full h-14 text-base font-normal items-center "
+                  )}
+                >
+                  <div className="flex gap-3 items-center truncate">
+                    <div className="flex flex-col">
+                      <span className="text-xs font-normal ">
+                        {messages.length > 0 ? messages[0].content : ""}
+                      </span>
+                    </div>
+                  </div>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="flex justify-end items-center"
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <MoreHorizontal size={15} className="shrink-0" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className=" ">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button
+                            variant="ghost"
+                            className="w-full flex gap-2 hover:text-red-500 text-red-500 justify-start items-center"
+                            onClick={(e) => e.stopPropagation()}
+                          >
+                            <Trash2 className="shrink-0 w-4 h-4" />
+                            Delete chat
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader className="space-y-4">
+                            <DialogTitle>Delete chat?</DialogTitle>
+                            <DialogDescription>
+                              Are you sure you want to delete this chat? This
+                              action cannot be undone.
+                            </DialogDescription>
+                            <div className="flex justify-end gap-2">
+                              <Button variant="outline">Cancel</Button>
+                              <Button
+                                variant="destructive"
+                                onClick={() => handleDeleteChat(chatId)}
+                              >
+                                Delete
+                              </Button>
+                            </div>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </Link>
+              ))}
+            </div>
+          )}
+          {isLoading && <SidebarSkeleton />}
+        </div>
+      </div>
+    </div>
+  );
 }
