0e22e7dd813816e590fcab5f837d13eeb8d993dc
henryouly <henryouly@gmail.com>
2024-05-30 06:36:40 +0000

    custom system prompt

--- stock-ai/app/api/chat/route.ts
+++ stock-ai/app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { StreamingTextResponse, Message, LangChainAdapter } from "ai";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import { AIMessage, HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
@@ -14,7 +15,18 @@ export async function POST(req: Request) {
     temperature: 0.8,
   });
 
-  const stream = await model
+  const chat_messages = (messages as Message[]).map((m) =>
+      m.role == "user"
+        ? new HumanMessage(m.content)
+        : new AIMessage(m.content)
+    );
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    new SystemMessage("You are a stock analyzer"),
+    ...chat_messages
+  ]);
+
+  const stream = await prompt.pipe(model)
     .stream(
       (messages as Message[]).map((m) =>
         m.role == "user"
